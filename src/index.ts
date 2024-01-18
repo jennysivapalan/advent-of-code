3f91273d1bd1b27a93816d1d53b9c863ad584e80
Jennifer Sivapalan <944375+jennysivapalan@users.noreply.github.com>
2024-01-18 16:58:04 +0000

    tidy: folder location for input data

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { total } from "./day1";
 import * as fs from "fs";
 
 //day 1 exercise
-const words = fs.readFileSync("src/day1-test.txt", "utf-8").split("\n");
+const words = fs.readFileSync("src/input-data/day1.txt", "utf-8").split("\n");
 const sum = total(words);
 console.log(sum);
